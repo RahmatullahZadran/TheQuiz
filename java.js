a361441519250b2c5f56d66d9a176c38b3bf45bb
Rahmatullah Zadran <rahmatullahzadran@outlook.com>
2023-12-30 22:46:44 +0000

    updated the backround and buttons

--- java.js
+++ java.js
@@ -8,20 +8,20 @@
 //   // You can also use other methods to display a message, such as console.log or modifying the DOM to show a message on the page
 // });
 
-const myButton = document.getElementById('myButton');
+// const myButton = document.getElementById('myButton');
 
-myButton.addEventListener('click', function() {
+// myButton.addEventListener('click', function() {
 
-    while (true) {
-        let name = prompt('Enter your name:');
-        if (name === 'jackon') {
-            alert('Hello, ' + name + '!');
-          let favoriteColor =  prompt('what is your favorite color?');
-            alert(favoriteColor1)
-            break;
-        } else {
-            alert('Invalid name. Please try again.');
-        }
+//     while (true) {
+//         let name = prompt('Enter your name:');
+//         if (name === 'jackon') {
+//             alert('Hello, ' + name + '!');
+//           let favoriteColor =  prompt('what is your favorite color?');
+//             alert(favoriteColor1)
+//             break;
+//         } else {
+//             alert('Invalid name. Please try again.');
+//         }
         
-    }
-})
+//     }
+// })
