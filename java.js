44b0adf63cdafdb6d2c5709c6ac08059facd573e
Rahmatullah Zadran <rahmatullahzadran@outlook.com>
2023-12-30 22:31:07 +0000

    updated all for better padding

--- java.js
+++ java.js
@@ -1,9 +1,27 @@
-// Get the button element
+// // Get the button element
+// const myButton = document.getElementById('myButton');
+
+// // Add a click event listener
+// myButton.addEventListener('click', function() {
+//   // Display a message when the button is clicked
+//   alert('Button clicked!');
+//   // You can also use other methods to display a message, such as console.log or modifying the DOM to show a message on the page
+// });
+
 const myButton = document.getElementById('myButton');
 
-// Add a click event listener
 myButton.addEventListener('click', function() {
-  // Display a message when the button is clicked
-  alert('Button clicked!');
-  // You can also use other methods to display a message, such as console.log or modifying the DOM to show a message on the page
-});
+
+    while (true) {
+        let name = prompt('Enter your name:');
+        if (name === 'jackon') {
+            alert('Hello, ' + name + '!');
+          let favoriteColor =  prompt('what is your favorite color?');
+            alert(favoriteColor1)
+            break;
+        } else {
+            alert('Invalid name. Please try again.');
+        }
+        
+    }
+})
