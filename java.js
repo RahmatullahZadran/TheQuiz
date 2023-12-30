6fa87d12f354bc48701b5d4e76f30c261c722093
Rahmatullah Zadran <rahmatullahzadran@outlook.com>
2023-12-30 21:57:40 +0000

    updating css,html and js to creat clickable buttons

---
+++ java.js
@@ -0,0 +1,9 @@
+// Get the button element
+const myButton = document.getElementById('myButton');
+
+// Add a click event listener
+myButton.addEventListener('click', function() {
+  // Display a message when the button is clicked
+  alert('Button clicked!');
+  // You can also use other methods to display a message, such as console.log or modifying the DOM to show a message on the page
+});
